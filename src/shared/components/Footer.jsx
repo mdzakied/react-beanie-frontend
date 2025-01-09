d1fa7a0df6f944693bd3dd70c7af554a733c49f5
Muhammad Dzaki Era Davala <mdzakied123@gmail.com>
2025-01-10 00:12:16 +0700

    feat: :zap: [element] (add footer)

--- src/shared/components/Footer.jsx
+++ src/shared/components/Footer.jsx
@@ -1,7 +1,170 @@
 export default function Footer() {
-    return (
-        <div>
-            <h1>Footer</h1>
+  // Menu Item
+  const menuItems1 = [
+    {
+      label: "HOME",
+      url: "/home",
+    },
+    {
+      label: "ABOUT",
+      url: "/about",
+    },
+    {
+      label: "SHOP",
+      url: "/shop",
+    },
+  ];
+
+  const menuItems2 = [
+    {
+      label: "SHOP SINGLE",
+      url: "/shop-single",
+    },
+    {
+      label: "BLOG",
+      url: "/blog",
+    },
+    {
+      label: "CONTACT",
+      url: "/contact",
+    },
+  ];
+
+  const menuItems3 = [
+    {
+      label: "TERMS & CONDITIONS",
+      disabled: true,
+    },
+    {
+      label: "SERVICES",
+      disabled: true,
+    },
+    {
+      label: "PRIVACY POLICY",
+      disabled: true,
+    },
+  ];
+
+  const menuItems4 = [
+    {
+      label: "ADDRESS, LOCATION 123",
+      url: "/address",
+    },
+    {
+      label: "YOURINFO@GMAIL.com",
+      url: "/email",
+    },
+    {
+      label: "111 33 44 555",
+      url: "/contact",
+    },
+  ];
+
+  return (
+    <div className="w-screen mt-4">
+      {/* Menu */}
+      <div className="grid">
+        {/* Menu 1 */}
+        <div className="col">
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {menuItems1.map((item, index) => (
+              <li key={index} style={{ padding: "2px 0" }}>
+                <a
+                  href={item.url}
+                  style={{
+                    textDecoration: "none",
+                    color: item.disabled ? "gray" : "black",
+                    padding: "5px 10px",
+                    display: "inline-block",
+                    pointerEvents: item.disabled ? "none" : "auto",
+                  }}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+        {/* Menu 2 */}
+        <div className="col">
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {menuItems2.map((item, index) => (
+              <li key={index} style={{ padding: "2px 0" }}>
+                <a
+                  href={item.url}
+                  style={{
+                    textDecoration: "none",
+                    color: item.disabled ? "gray" : "black",
+                    padding: "5px 10px",
+                    display: "inline-block",
+                    pointerEvents: item.disabled ? "none" : "auto",
+                  }}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Menu 3 */}
+        <div className="col">
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {menuItems3.map((item, index) => (
+              <li key={index} style={{ padding: "2px 0" }}>
+                <a
+                  href={item.url}
+                  style={{
+                    textDecoration: "none",
+                    color: item.disabled ? "gray" : "black",
+                    padding: "5px 10px",
+                    display: "inline-block",
+                    pointerEvents: item.disabled ? "none" : "auto",
+                  }}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Menu 4 */}
+        <div className="col">
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {menuItems4.map((item, index) => (
+              <li key={index} style={{ padding: "2px 0" }}>
+                <a
+                  href={item.url}
+                  style={{
+                    textDecoration: "none",
+                    color: item.disabled ? "gray" : "black",
+                    padding: "5px 10px",
+                    display: "inline-block",
+                    pointerEvents: item.disabled ? "none" : "auto",
+                  }}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {/* Copyright */}
+      <div className="border-top-1">
+        <p className="text-center text-sm mb-0">
+          {" "}
+          &copy; 2023 Beanie. Designed by{" "}
+          <span>
+            <a
+              href="https://templatesjungle.com/"
+              target="_blank"
+              className="no-underline"
+            >
+              TemplateJungle
+            </a>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 }
