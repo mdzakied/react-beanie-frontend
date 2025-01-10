6e403e8da2e34fd0ffbb8fb670bcc4b08b7ea74d
Muhammad Dzaki Era Davala <mdzakied123@gmail.com>
2025-01-10 14:03:37 +0700

    feat: :zap: [element] (add error page)

--- src/shared/pages/Error404.jsx
+++ src/shared/pages/Error404.jsx
@@ -1,7 +1,7 @@
 export default function Error404() {
   return (
-    <div className="card flex justify-content-center items-center fixed top-0 left-0 w-screen h-screen bg-white z-50">
-      <p className=" text-xl font-bold text-red-600 my-auto">Not Found Page</p>
+    <div className="flex justify-content-center items-center fixed top-0 left-0 w-screen h-screen bg-white z-50">
+      <h1 className="font-bold  color-tertiary my-auto">404 Not Found Page</h1>
     </div>
   );
 }
