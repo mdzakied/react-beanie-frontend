66a5fd04f12e219498dcd63efb427ff17ad26efc
Muhammad Dzaki Era Davala <mdzakied123@gmail.com>
2025-01-10 13:22:28 +0700

    feat: :zap: [element] (fix footer)

--- src/shared/components/Footer.jsx
+++ src/shared/components/Footer.jsx
@@ -78,6 +78,7 @@ export default function Footer() {
                     display: "inline-block",
                     pointerEvents: item.disabled ? "none" : "auto",
                   }}
+                  className="hover:text-yellow-600 cursor-pointer"
                 >
                   {item.label}
                 </a>
@@ -99,6 +100,7 @@ export default function Footer() {
                     display: "inline-block",
                     pointerEvents: item.disabled ? "none" : "auto",
                   }}
+                  className="hover:text-yellow-600 cursor-pointer"
                 >
                   {item.label}
                 </a>
@@ -120,6 +122,7 @@ export default function Footer() {
                     display: "inline-block",
                     pointerEvents: item.disabled ? "none" : "auto",
                   }}
+                  className="hover:text-yellow-600 cursor-pointer"
                 >
                   {item.label}
                 </a>
@@ -141,6 +144,7 @@ export default function Footer() {
                     display: "inline-block",
                     pointerEvents: item.disabled ? "none" : "auto",
                   }}
+                  className="hover:text-yellow-600 cursor-pointer"
                 >
                   {item.label}
                 </a>
