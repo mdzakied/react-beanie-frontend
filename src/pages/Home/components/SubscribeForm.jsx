6ceb983bf8d39464d2881b6ff9c60d07c1ecd869
Muhammad Dzaki Era Davala <mdzakied123@gmail.com>
2025-01-10 13:19:48 +0700

    feat: :zap: [element] (add subscribe form)

--- src/pages/Home/components/SubscribeForm.jsx
+++ src/pages/Home/components/SubscribeForm.jsx
@@ -1,7 +1,35 @@
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+
 export default function SubscribeForm() {
   return (
-    <div>
-      <h1>Subscribe Form</h1>
+    <div className="flex flex-column justify-content-center align-items-center h-full my-4">
+      {/* Subscribe Title */}
+      <h1 className="mb-0 color-tertiary">SUBSCRIBE US</h1>
+      <p className="text-center font-secondary">
+        Subscribe to our newsletterfor discount codes and updates
+      </p>
+      {/* Subscribe Form */}
+      <div className="my-3 flex">
+        <InputText
+          placeholder="Write your email address here..."
+          className="input-subs-custom mx-auto"
+        />
+        <Button
+          className="btn-subs-custom color-tertiary font-primary text-sm border-2"
+          label="ORDER NOW"
+          rounded
+          outlined
+        />
+      </div>
+      {/* Social Media */}
+      <div className="flex justify-content-center gap-3 my-3">
+        <i className="pi pi-facebook hover:text-yellow-600 cursor-pointer"></i>
+        <i className="pi pi-instagram hover:text-yellow-600 cursor-pointer"></i>
+        <i className="pi pi-twitter hover:text-yellow-600 cursor-pointer"></i>
+        <i className="pi pi-linkedin hover:text-yellow-600 cursor-pointer"></i>
+        <i className="pi pi-youtube hover:text-yellow-600 cursor-pointer"></i>
+      </div>
     </div>
   );
 }
