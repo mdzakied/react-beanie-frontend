116a89501672e273c6a3a6b89baf2ef899a21257
Muhammad Dzaki Era Davala <mdzakied123@gmail.com>
2025-01-10 09:41:32 +0700

    feat: :zap: [element] (add description)

--- src/pages/Home/components/Description.jsx
+++ src/pages/Home/components/Description.jsx
@@ -1,7 +1,12 @@
 export default function Description() {
   return (
-    <div>
-      <h1>Description</h1>
+    <div className="card flex justify-content-center text-center my-6">
+      <p className="mx-auto w-8 text-xl font-italic line-height-3">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum qui sunt
+        itaque alias aperiam. Saepe porro quisquam quidem, suscipit aliquam
+        aliquid neque tenetur earum fuga tempora optio molestiae pariatur
+        perspiciatis.
+      </p>
     </div>
   );
 }
